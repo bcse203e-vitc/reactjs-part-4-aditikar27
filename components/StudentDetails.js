ece74b8e704419f33811d2a4dff9ce045f98ed42
Aditi Kar <aditi.kar2023@vitstudent.ac.in>
2025-03-11 15:26:30 +0530

    Add files via upload
    adding incomplete component files of question 1

---
+++ components/StudentDetails.js
@@ -0,0 +1,21 @@
+const students = [{
+    age: 19,
+    name: 'Saumya Uberoi',
+    grade: 'S',
+  }, {
+     age: 20,
+    name: 'Shrey Maity',
+    grade: 'A',
+  }, {
+     age: 19,
+    name: 'Aditi Kar',
+    grade: 'D',
+  }, {
+     age: 20,
+    name: 'Aarushi Saxena',
+    grade: 'S',
+  }, {
+     age: 21,
+    name: 'Prerna Pyassi',
+    grade: 'A',
+  }];
